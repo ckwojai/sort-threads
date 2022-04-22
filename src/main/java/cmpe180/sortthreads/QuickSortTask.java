8e21b5097fb174533ef6f2b67905ef72f7b4a811
Kin Chang <kinchang@Kins-MacBook-Pro.local>
2022-04-22 13:57:57 -0700

    Add sortthreads package and QuickSortTask class and test

---
+++ src/main/java/cmpe180/sortthreads/QuickSortTask.java
@@ -0,0 +1,29 @@
+package cmpe180.sortthreads;
+
+import java.util.concurrent.RecursiveAction;
+
+public class QuickSortTask extends RecursiveAction {
+    @Override
+    protected void compute() {
+
+    }
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(convert(i));
+        }
+    }
+
+    public static String convert(int fizzBuzz) {
+        if (fizzBuzz % 15 == 0) {
+            return "FizzBuzz";
+        }
+        if (fizzBuzz % 3 == 0) {
+            return "Fizz";
+        }
+        if (fizzBuzz % 5 == 0) {
+            return "Buzz";
+        }
+        return String.valueOf(fizzBuzz);
+    }
+}
---
+++ src/test/java/cmpe180/sortthreads/QuickSortTest.java
@@ -0,0 +1,37 @@
+package cmpe180.sortthreads;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class QuickSortTest {
+    @Test
+    public void QuickSortNormalNumbers() {
+
+        QuickSortTask fb = new QuickSortTask();
+        Assertions.assertEquals("1", fb.convert(1));
+        Assertions.assertEquals("2", fb.convert(2));
+    }
+
+    @Test
+    public void QuickSortThreeNumbers() {
+
+        QuickSortTask fb = new QuickSortTask();
+        Assertions.assertEquals("Fizz", fb.convert(3));
+    }
+
+    @Test
+    public void QuickSortFiveNumbers() {
+
+        QuickSortTask fb = new QuickSortTask();
+        Assertions.assertEquals("Buzz", fb.convert(5));
+    }
+
+    @Test
+    public void QuickSortThreeAndFiveNumbers() {
+
+        QuickSortTask fb = new QuickSortTask();
+        Assertions.assertEquals("Buzz", fb.convert(5));
+    }
+}
