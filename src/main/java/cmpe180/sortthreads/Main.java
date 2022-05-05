0f94cca035fc35c687c8018c1e17eedbd6286fe9
Kyra Foerster <kyra.foerster@web.de>
2022-05-05 16:17:35 -0700

    add QuickSort Task with comparable IF, main needs fix

---
+++ src/main/java/cmpe180/sortthreads/Main.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import org.example.QuickSortTask;
+import java.util.Arrays;
+import java.util.concurrent.ForkJoinPool;
+
+public class Main {
+  public static void main(String[] args) {
+    ForkJoinPool fjPool = new ForkJoinPool();
+    int[] a = new int[3333344];
+    
+
+    
+
+    for (int i = 0; i < a.length; i++) {
+      int k = (int) (Math.random() * 22222);
+      a[i] = k;
+    }
+    QuickSortTask<int> forkJoinQuicksortTask = new QuickSortTask<int>(a,0, a.length - 1);
+    long start = System.nanoTime();
+    fjPool.invoke(forkJoinQuicksortTask);
+    System.out.println("Time: " + (System.nanoTime() - start));
+  }
+}
+
+
+
+
+
+
+
+
+
+
--- src/main/java/cmpe180/sortthreads/QuickSortTask.java
+++ src/main/java/cmpe180/sortthreads/QuickSortTask.java
@@ -1,28 +1,73 @@
-package cmpe180.sortthreads;
+package org.example;
 
+import java.util.Arrays;
 import java.util.concurrent.RecursiveAction;
 
-public class QuickSortTask extends RecursiveAction {
-    @Override
-    protected void compute() {
-    }
 
-    public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++) {
-            System.out.println(convert(i));
-        }
-    }
+class QuickSortTask<T extends Comparable> extends RecursiveAction {
 
-    public static String convert(int fizzBuzz) {
-        if (fizzBuzz % 15 == 0) {
-            return "FizzBuzz";
-        }
-        if (fizzBuzz % 3 == 0) {
-            return "Fizz";
-        }
-        if (fizzBuzz % 5 == 0) {
-            return "Buzz";
-        }
-        return String.valueOf(fizzBuzz);
+  // Define class variables
+  T a[];
+  
+  int left;
+  int right;
+
+  // Define  and implement constructors
+  //public QuickSortTask(ArrayList<T> a) {
+   // this(a, 0, a.size()- 1);
+  //}
+  public QuickSortTask(T[] a) {
+    this(a, 0, a.length- 1);
+  }
+
+  // Paramatrized constructor
+  public QuickSortTask(T[] a, int left, int right) {
+    this.a = a;
+    this.left = left;
+    this.right = right;
+  }
+
+  // Threshold given via project task
+  static int THRESHOLD = 100;
+  @Override
+  protected void compute() {
+      // if size of sublist < 100, then sort directly
+    if ((right-left)<THRESHOLD) {
+      //Collections.sort(a.subList(left, right));
+      Arrays.sort(a, left, right + 1);
+    } else {
+        // otherwise create subtasks
+      int pivotIndex = partition(a, left, right);
+      QuickSortTask t1 = new QuickSortTask(a, left,
+          pivotIndex - 1);
+      QuickSortTask t2 = new QuickSortTask(a, pivotIndex + 1,
+          right);
+      t1.fork();
+      t2.compute();
+      t1.join();
     }
+  }
+
+  int partition(T[] a, int p, int r) {
+    int i = p - 1;
+    int res;
+    T x = a[r];
+    for (int j = p; j < r; j++) {
+      //if (a[j] < x) {
+      res= a[j].compareTo(x); // return -1 if <
+      if (res == -1){
+        i++;
+        swap(a, i, j);
+      }
+    }
+    i++;
+    swap(a, i, r);
+    return i;
+  }
+
+  void swap(T[] a, int p, int r) {
+    T t = a[p];
+    a[p] = a[r];
+    a[r] = t;
+  }
 }
