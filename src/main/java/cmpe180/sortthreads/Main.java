b36a8db273b30c12010aa41705329fef22d6394d
Kyra Foerster <kyra.foerster@web.de>
2022-05-05 22:08:34 -0700

    main and quicksort work

--- src/main/java/cmpe180/sortthreads/Main.java
+++ src/main/java/cmpe180/sortthreads/Main.java
@@ -7,16 +7,14 @@ import java.util.concurrent.ForkJoinPool;
 public class Main {
   public static void main(String[] args) {
     ForkJoinPool fjPool = new ForkJoinPool();
-    int[] a = new int[3333344];
-    
-
-    
+    Integer[] a = new Integer[3333344];
 
     for (int i = 0; i < a.length; i++) {
       int k = (int) (Math.random() * 22222);
       a[i] = k;
     }
-    QuickSortTask<int> forkJoinQuicksortTask = new QuickSortTask<int>(a,0, a.length - 1);
+
+    QuickSortTask<Integer> forkJoinQuicksortTask = new QuickSortTask<>(a,0, a.length - 1);
     long start = System.nanoTime();
     fjPool.invoke(forkJoinQuicksortTask);
     System.out.println("Time: " + (System.nanoTime() - start));
