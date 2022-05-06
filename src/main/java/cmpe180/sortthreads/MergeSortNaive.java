973b53d68cf692e5f827dc0c809fa99fc46b8430
Kin Chang <kinchang@Kins-MacBook-Pro.local>
2022-05-06 16:24:27 -0700

    benchmark for mergeSort

---
+++ src/main/java/cmpe180/sortthreads/MergeSortNaive.java
@@ -0,0 +1,25 @@
+package cmpe180.sortthreads;
+
+import java.util.Arrays;
+
+public class MergeSortNaive<T extends Comparable<T>> {
+    static final int THRESHOLD = 100;
+    protected void sort(T[] arr, int l, int r) {
+        if (r-l < THRESHOLD) {
+            Arrays.sort(arr, l, r);
+        } else {
+            int mid = (l+r)  >>> 1;
+            sort(arr, l, mid);
+            sort(arr, mid, r);
+            merge(arr, l, mid, r);
+        }
+    }
+    void merge(T[] arr, int l, int mid, int r) {
+        T[] buffer = Arrays.copyOfRange(arr, l, mid);
+        for(int i=0, j=l, k=mid; i < buffer.length; j++) {
+            T smallerElement = (k == r || buffer[i].compareTo(arr[k]) < 0 ? buffer[i++] : arr[k++]);
+            arr[j]= smallerElement;
+        }
+    }
+
+}
--- src/test/java/cmpe180/sortthreads/MergeSortTest.java
+++ src/test/java/cmpe180/sortthreads/MergeSortTest.java
@@ -11,20 +11,65 @@ class MergeSortTest {
     public void MergeSortSimpleTest() {
         final int n = 10000;
         final int max_limit = 100000;
-//        final int n = 20;
-//        final int max_limit = 20;
         Random rand = new Random();
         Integer[] inputArray = new Integer[n];
         for(int i=0; i<n; i++) {
             Integer r = rand.nextInt(max_limit);
             inputArray[i] = r;
         }
-        Integer[] sortedArray = inputArray.clone();
-        Arrays.sort(sortedArray);
+        Integer[] expectedArray = inputArray.clone();
+        Arrays.sort(expectedArray);
 
         ForkJoinPool pool = new ForkJoinPool();
         MergeSortTask task = new MergeSortTask<Integer>(inputArray);
         pool.invoke(task);
-        Assertions.assertArrayEquals(sortedArray, inputArray);
+        Assertions.assertArrayEquals(expectedArray, inputArray);
+    }
+    @Test
+    public void MergeSortNaiveTest() {
+        final int n = 10000;
+        final int max_limit = 100000;
+        Random rand = new Random();
+        Integer[] inputArray = new Integer[n];
+        for(int i=0; i<n; i++) {
+            Integer r = rand.nextInt(max_limit);
+            inputArray[i] = r;
+        }
+        Integer[] expectedArray = inputArray.clone();
+        Arrays.sort(expectedArray);
+        MergeSortNaive msn = new MergeSortNaive<Integer>();
+        msn.sort(inputArray, 0, inputArray.length);
+        Assertions.assertArrayEquals(expectedArray, inputArray);
+    }
+
+    @Test
+    public void MergeSortBenchMark() {
+        int[] ns = {(int) 1.0e4, (int) 1.0e5, (int) 1.0e6, (int) 1.0e7};
+        for (int j=0; j < ns.length; j++) {
+            int n = ns[j];
+            final int max_limit = n;
+            Random rand = new Random();
+            Integer[] inputThreaded = new Integer[n];
+            for (int i = 0; i < n; i++) {
+                Integer r = rand.nextInt(max_limit);
+                inputThreaded[i] = r;
+            }
+            Integer[] inputNaive = inputThreaded.clone();
+
+            ForkJoinPool pool = new ForkJoinPool();
+            MergeSortTask task = new MergeSortTask<Integer>(inputThreaded);
+
+            long startTime = System.nanoTime();
+            pool.invoke(task);
+            long endTime = System.nanoTime();
+            long threadedDuration = (endTime - startTime) / 1000000;
+
+            startTime = System.nanoTime();
+            MergeSortNaive msn = new MergeSortNaive<Integer>();
+            msn.sort(inputNaive, 0, inputNaive.length);
+            endTime = System.nanoTime();
+            long naiveDuration = (endTime - startTime) / 1000000;
+            System.out.println("For n=" + n + ",   " + "Naive: " + naiveDuration + "; Threaded: " + threadedDuration);
+        }
     }
 }
